0c0b833803562fec7c92f6e79f7402ab7e48e4ea
Ashima <96079099+Ashimapuri@users.noreply.github.com>
2022-08-27 14:36:28 +0530

    Update Pokemoninfo.jsx

--- pokedex/src/Components/Pokemoninfo.jsx
+++ pokedex/src/Components/Pokemoninfo.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-const Pokemoninfo=()=>{
+const Pokemoninfo=({data})=>{
+    console.log(data)
     return(
         <>
+        <h1>1</h1>
+        <img src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/id.svg" alt=" "/>
         </>
     )
 }
