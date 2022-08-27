495df3143e70755ff0c66b6960f4c2b9cc5a8018
Ashima <96079099+Ashimapuri@users.noreply.github.com>
2022-08-27 14:17:29 +0530

    Update Card.jsx

---
+++ pokedex/src/Components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Card=()=>{
+    return(
+        <>
+        <div className="card">
+            <h2>1</h2>
+            <img src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/id.svg" alt=" " />
+            <div className="abilities">
+                <h2></h2>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Card;
